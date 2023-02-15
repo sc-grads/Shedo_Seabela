ffe59b4f02ef3474d775a6e3a3add4cb2ec67964
SeabelaSHEDO <shedo.seabela@sambeconsulting.com>
2023-02-15 11:12:45 +0200

    functions docstring

---
+++ Python_Bootcamp/Functions/Functions.py
@@ -0,0 +1,13 @@
+# functins
+def my_function():
+    print('Hello world')
+
+my_function()
+
+def say_hi(name):
+    """ this is a function to great you
+
+    """
+    print(f'Hello {name} how are you')
+
+say_hi('Dell')
