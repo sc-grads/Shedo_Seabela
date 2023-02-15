91e918295cafc2eeadc056379254aa8951783bb0
SeabelaSHEDO <shedo.seabela@sambeconsulting.com>
2023-02-15 14:33:05 +0200

    functions coding Challeges

--- Python_Bootcamp/Functions/Functions.py
+++ Python_Bootcamp/Functions/Functions.py
@@ -42,3 +42,68 @@ def increment():
 # Calling the function
 increment()
 print(x)
+
+
+# Challenges
+def unique(Sample_list = []):
+
+    new_list = list(set(Sample_list))
+    print(new_list)
+
+unique([1, 2, 3, 3, 3, 3, 4, 5, 1, 3, 5, 5, 5])
+
+def all_divisors(n):
+    divisors = []
+    for x in range(1, int(n/2)+1):
+        if n % x == 0:
+            divisors.append(x)
+    return divisors
+
+
+def perfect_number(n):
+    divs = all_divisors(n)
+    if sum(divs) == n:
+        return True
+    else:
+        return False
+
+n = 496
+if perfect_number(n):
+    print(f'{n} it\'s a perfect number')
+else:
+    print(f'Nothing special about {n}, it\'s no perfect number')
+
+fac = 1
+def factorial(n):
+    for i in range(1, n+1):
+        global fac
+        fac = fac * i
+
+    return fac
+
+print(factorial(3))
+
+def prime(n):
+    divisors = []
+    for x in range(1, int(n / 2) + 1):
+        if n % x == 0:
+            divisors.append(x)
+    if len(divisors) <= 2:
+        return True
+    else:
+        pass
+prime(8)
+print("DONE WITH FIRST FUNCTIONS")
+primes = []
+for n in range(1_000_000, 100_000_000):
+   if prime(n):
+       primes.append(n)
+       if len(primes) == 5:
+           break
+print(primes)
+
+
+
+
+
+
