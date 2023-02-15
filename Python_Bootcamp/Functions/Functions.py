2bf48e5d25f82879f0ea6aefc1197cc4085e3bfe
SeabelaSHEDO <shedo.seabela@sambeconsulting.com>
2023-02-15 11:51:31 +0200

    functions coding task

--- Python_Bootcamp/Functions/Functions.py
+++ Python_Bootcamp/Functions/Functions.py
@@ -17,3 +17,16 @@ def difference(a, b):
     print(x)
 
 difference(8, 5)
+
+
+# Write a function called get_vat() with 2 parameters, price and vat percentage
+# It calculates and returns the VAT (value-added tax).
+# YOUR CODE STARTS HERE
+def get_vat(price, vat_percentage):
+    vat = price * vat_percentage / 100
+    return vat
+
+
+get_vat(200, 5)
+
+# Capture the returned value in a new variable called vat.
