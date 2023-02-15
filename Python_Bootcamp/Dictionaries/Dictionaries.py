d05300ad2ee84c32fe8fab5591070b3b8554d667
SeabelaSHEDO <shedo.seabela@sambeconsulting.com>
2023-02-15 09:30:53 +0200

    Dictionaries coding quiz

---
+++ Python_Bootcamp/Dictionaries/Dictionaries.py
@@ -0,0 +1,5 @@
+# Create a dictionary called user with 4 key: value pairs
+user = {'name': 'Shedo', 'age': 25, 'location': "Kempton park", 'email': 'ShedoSeabela.com'}
+
+song = {'artist': 'John Winston Ono Lennon', 'name': 'Imagine', 'year': 1971}
+
