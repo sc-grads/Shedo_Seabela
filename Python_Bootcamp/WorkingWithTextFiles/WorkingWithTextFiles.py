92d145486ba97c1a756e28b031f725e0a5068a7e
SeabelaSHEDO <shedo.seabela@sambeconsulting.com>
2023-02-16 08:04:12 +0200

    Working with text files project files

--- Python_Bootcamp/WorkingWithTextFiles/WorkingWithTextFiles.py
+++ Python_Bootcamp/WorkingWithTextFiles/WorkingWithTextFiles.py
@@ -6,8 +6,30 @@ print(content.closed)
 
 # YOUR CODE STARTS HERE
 
-ip_list = open('ip.txt')
-ip_list = ip_list.read().splitlines()
+# ip_list = open('ip.txt')
+# ip_list = ip_list.read().splitlines()
+
+# writing on a file
+n_file = open('new_text.txt', 'w')
+n_file.write('this is a  new file')
+n_file.write('\nThis is a second line')
 
 
+n_file = open('new_text.txt', 'a')
+n_file.write('\nthis is a third line with append mode')
+n_file.write('\nThis is a second line')
 
+
+n_file = open('new_text.txt', 'w')
+n_file.write('\nthis is a  new file\n')
+n_file.write('\nThis is a second line')
+
+# project
+fileS = open('devices.txt')
+data = fileS.read().splitlines()
+print(data)
+devices = list()
+for i in data:
+    devices.append(i.split(':'))
+
+print(devices)
