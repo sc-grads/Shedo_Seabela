eb764500500cd1490d1ed7797b92100a79c6e6f6
SeabelaSHEDO <shedo.seabela@sambeconsulting.com>
2023-02-16 16:24:40 +0200

    PythonModules

--- Python_Bootcamp/WorkingWtihPythonModules/WorkingWtihPythonModules.py
+++ Python_Bootcamp/WorkingWtihPythonModules/WorkingWtihPythonModules.py
@@ -29,3 +29,68 @@ import math as my_math
 
     # Importing everything from random module directly
 from random import *
+## Importing the module
+import sys
+
+## Returns the version of the Python interpreter
+sys.version
+
+## Other info about the interpreter
+sys.implementation
+
+## Returns the platform (linux, win32 or mac)
+sys.platform
+
+## Returns the PATH (directories where it searches for modules)
+sys.path
+
+my_list = list(range(100))
+sys.getsizeof(my_list)  # returns the size of the list in memory
+
+## Redirecting output to a file instead of stdout (console)
+original_stdout = sys.stdout  # saving the original stdout
+
+## Using a file as stdout, redirecting output to a file
+with open('stdout.txt', 'w') as sys.stdout:
+    print('Printing a string')
+    x = 10
+    print(f'x is {x}')
+
+sys.stdout = original_stdout
+
+import os
+
+## IMPORTANT!
+## You should always use a valid path according to your operating system
+
+
+## Returns the file that defines the module
+os.__file__  # => '/usr/lib/python3.7/os.py'
+
+cwd = os.getcwd()  # => returns the current working directory
+print(cwd)
+
+## changes the current working directory, OS specific
+os.chdir('/home/user1/python')  # Linux specific
+os.chdir('C:\\Users')  # Windows specific
+
+os.path.isfile('/etc/passwd')  # returns True if argument is a file
+os.path.isdir('/tmp')  # returns True if argument is a directory
+
+## Splits the extension from a pathname, returns a tuple
+name, extention = os.path.splitext('/var/log/kern.log')  # name is 'kern', extention is '.log'
+
+## Listing a directory
+os.listdir('.')  # returns the entries in the current working directory as a list
+os.listdir('C:\\Users')  # returns the entries in the directory as a list
+
+os.path.getmtime('/etc/passwd')  # => 1547047769.9049096, returns file's last modification time as a timestamp
+os.path.getsize('/etc/passwd')  # => 2691, returns the size of a file
+
+os.mkdir('PATH/dir')  # creates a new directory in PATH, os specific
+
+## Creates a new directory called dir1 in Windows (permissions required)
+os.mkdir('C:\\dir1')
+
+## Creates a new directory called dir1 in Linux
+os.mkdir('~/dir1')
