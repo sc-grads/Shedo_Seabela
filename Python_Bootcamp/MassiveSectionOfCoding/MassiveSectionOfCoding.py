e16ba919e242b2d75db8e0c1d37fc08a81acd89b
SeabelaSHEDO <shedo.seabela@sambeconsulting.com>
2023-02-16 14:57:44 +0200

    19

--- Python_Bootcamp/MassiveSectionOfCoding/MassiveSectionOfCoding.py
+++ Python_Bootcamp/MassiveSectionOfCoding/MassiveSectionOfCoding.py
@@ -127,3 +127,33 @@ def counter(my_str):
 
 print(counter('Python'))
 print(counter('BeautifUl'))
+
+countries = ['USA', 'UK', 'France', 'Romania', 'France', 'Germany', 'USA', 'Canada', 'India', 'UK']
+
+# Removing duplicates from list by transforming it to a set and then back to a list
+countries = list(set(countries))
+countries.sort()
+
+print(countries)
+
+with open('show_arp.txt', 'r', newline='') as f:
+    # Reading file in list (each line is list element)
+    contents = f.read().splitlines()
+
+    # The argument newline='' is necessary only in Windows
+
+    # Eliminating the first item from the list (files header)
+    contents = contents[1:]
+
+    # Empty list that stores tuples like (ip, mac)
+    ip_mac = list()
+
+    # Iterating over the list(file contents) line by line
+    for line in contents:
+        ip = line.split()[1]  ## Getting the IP
+        mac = line.split()[3]  ## Getting the MAC
+
+        # Adding the tuple to the ip_mac list
+        ip_mac.append((ip, mac))
+
+    print(ip_mac)
