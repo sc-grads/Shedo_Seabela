aa20c3ca4564ad64d6881084b96a72d29af0ce77
SeabelaSHEDO <shedo.seabela@sambeconsulting.com>
2023-02-16 11:29:48 +0200

    Introduction

---
+++ Python_Bootcamp/OOP/OOP.py
@@ -0,0 +1,17 @@
+from turtle import Turtle, Screen
+my_screen = Screen()
+Seabela = Turtle()
+print(my_screen.canvwidth)
+Seabela.shape('turtle')
+Seabela.color('purple')
+Seabela.forward(100)
+Seabela.right(90)
+Seabela.forward(100)
+Seabela.right(90)
+Seabela.forward(100)
+Seabela.right(90)
+Seabela.forward(200)
+Seabela.home()
+
+
+my_screen.exitonclick()
