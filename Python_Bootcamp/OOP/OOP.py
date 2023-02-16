6e1df1c5da72b5f64a5b5916777525f63ffde30b
SeabelaSHEDO <shedo.seabela@sambeconsulting.com>
2023-02-16 11:43:20 +0200

    Introduction with fixed error

--- Python_Bootcamp/OOP/OOP.py
+++ Python_Bootcamp/OOP/OOP.py
@@ -1,17 +1,28 @@
-from turtle import Turtle, Screen
-my_screen = Screen()
-Seabela = Turtle()
-print(my_screen.canvwidth)
-Seabela.shape('turtle')
-Seabela.color('purple')
-Seabela.forward(100)
-Seabela.right(90)
-Seabela.forward(100)
-Seabela.right(90)
-Seabela.forward(100)
-Seabela.right(90)
-Seabela.forward(200)
-Seabela.home()
+# from turtle import Turtle, Screen
+# my_screen = Screen()
+# Seabela = Turtle()
+# print(my_screen.canvwidth)
+# Seabela.shape('turtle')
+# Seabela.color('purple')
+# Seabela.forward(100)
+# Seabela.right(90)
+# Seabela.forward(100)
+# Seabela.right(90)
+# Seabela.forward(100)
+# Seabela.right(90)
+# Seabela.forward(200)
+# Seabela.home()
+#
+#
+# my_screen.exitonclick()
 
+class Robot:
+    """This class implements a robot"""
+    def __init__(self, name, year):
+        self.name = name
+        self.year = year
 
-my_screen.exitonclick()
+r1 = Robot('R1', 2023)
+print(r1.__doc__)
+print(f'Robot name: {r1.name}')
+print(r1.__doc__)
