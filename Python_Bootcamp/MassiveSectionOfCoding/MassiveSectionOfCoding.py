e8a9249817bcc3872cc283c35235f3e558523e44
SeabelaSHEDO <shedo.seabela@sambeconsulting.com>
2023-02-16 14:12:19 +0200

    2

--- Python_Bootcamp/MassiveSectionOfCoding/MassiveSectionOfCoding.py
+++ Python_Bootcamp/MassiveSectionOfCoding/MassiveSectionOfCoding.py
@@ -3,3 +3,5 @@ number_of_ip = 2 ** 128
 no_of_digits = len(str(number_of_ip))
 print(no_of_digits)
 
+phone = {'Brand':'Samsung', 'Price':650.2, 'Seller': 'Nile'}
+phone.update({'OS':'Android', 'Color': 'Black'})
