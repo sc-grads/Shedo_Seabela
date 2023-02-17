3087682e507a9e7865966dd6b784cfc10b4dfacc
SeabelaSHEDO <shedo.seabela@sambeconsulting.com>
2023-02-17 15:02:10 +0200

    Writing to csv files

--- Python_Bootcamp/WorkingWithCSV/WorkingWithCSV.py
+++ Python_Bootcamp/WorkingWithCSV/WorkingWithCSV.py
@@ -4,3 +4,16 @@ with open('airtravel.csv', 'r') as content:
     next(reader)
     for row in reader:
         print(row[3])
+
+
+with open('people.csv', 'a') as csvfile:
+    writer = csv.writer(csvfile)
+    csvdata = (5, 'Anne', 'Amsterdam')
+    writer.writerow(csvdata)
+
+
+with open('numbers.csv', 'w', newline='') as f:
+    writer = csv.writer(f)
+    writer.writerow(['x', 'x**2', 'x**3', 'x**4'])
+    for x in range(1, 101):
+        writer.writerow([x, x ** 2, x ** 3, x ** 4])
