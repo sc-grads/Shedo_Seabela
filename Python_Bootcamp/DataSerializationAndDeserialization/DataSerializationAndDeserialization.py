faa6b487b9643d85ac4de26eab6d188d05623f57
SeabelaSHEDO <shedo.seabela@sambeconsulting.com>
2023-02-17 16:19:28 +0200

    challenge 2

--- Python_Bootcamp/DataSerializationAndDeserialization/DataSerializationAndDeserialization.py
+++ Python_Bootcamp/DataSerializationAndDeserialization/DataSerializationAndDeserialization.py
@@ -1,4 +1,6 @@
 import pickle, json
+import requests
+import csv
 friends = {"Dan": (20, "London", 13242252), "Maria": [25, "Madrid", 34232424]}
 with open('friends.dat', 'wb') as f:
     pickle.dump(friends, f)
@@ -92,3 +94,31 @@ if __name__ == "__main__":
     x = deserialize('a.json', 'json')
 
     print(f'json: {x}')
+
+response = requests.get("https://jsonplaceholder.typicode.com/users")
+
+# loading the json encoded string into a Python Object
+# users it's a list of dictionaries
+users = json.loads(response.text)
+
+# opening the csv file for writing
+with open('users.csv', 'w') as f:
+    writer = csv.writer(f)
+
+    # write a header to file
+    writer.writerow(("Name", "City", "GPS", "Company"))
+
+    # iterating over the users list
+    for user in users:
+        # getting the data from the dictionary
+        name = user['name']
+        city = user['address']['city']
+        lat = user['address']['geo']['lat']
+        lng = user['address']['geo']['lng']
+        # constructing the GPS coordinates in form of (lat, lng)
+        geo = f'({lat},{lng})'
+        company_name = user['company']['name']
+
+        # writing to csv file
+        csv_data = (name, city, geo, company_name)
+        writer.writerow(csv_data)
