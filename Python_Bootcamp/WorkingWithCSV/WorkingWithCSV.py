18b6bdb4272fa48026222d3dd7232b0c01450288
SeabelaSHEDO <shedo.seabela@sambeconsulting.com>
2023-02-17 15:17:27 +0200

    Challenge 1

--- Python_Bootcamp/WorkingWithCSV/WorkingWithCSV.py
+++ Python_Bootcamp/WorkingWithCSV/WorkingWithCSV.py
@@ -17,3 +17,22 @@ with open('numbers.csv', 'w', newline='') as f:
     writer.writerow(['x', 'x**2', 'x**3', 'x**4'])
     for x in range(1, 101):
         writer.writerow([x, x ** 2, x ** 3, x ** 4])
+
+people = [
+    ['Dan', 34, 'Bucuresti'],
+    ['Andrei',21, 'London'],
+    ['Maria', 45, 'Paris']
+]
+
+
+with open('people1.csv', 'w') as f:
+    writer = csv.writer(f)
+    for item in people:
+        writer.writerow(item)
+
+
+
+with open('people1.csv') as f:
+    reader = csv.reader(f)
+    for row in reader:
+        print(row)
