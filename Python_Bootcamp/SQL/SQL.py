fcbd213f9b722923d2f4d372d6189c04e95682b2
SeabelaSHEDO <shedo.seabela@sambeconsulting.com>
2023-02-20 06:12:14 +0200

    updating

--- Python_Bootcamp/SQL/SQL.py
+++ Python_Bootcamp/SQL/SQL.py
@@ -3,18 +3,9 @@ import sqlite3
 connection = sqlite3.connect('my_database.db')
 
 cursor = connection.cursor()
-id = input('Enter ID:')
-sql = 'select * from employees where id = ?;'
+id = input('Id:')
+
+sql = 'UPDATE employees SET department="Sales" where id=?;'
 cursor.execute(sql, (id,))
-
-for row in cursor.fetchall():
-    print(row)
-
-
-record = (10, 'Leonardo Romano', 'Marketing', '+40122111', 'leo@x.com')
-
-sql = 'insert into employees values (?,?,?,?,?);'
-cursor.execute(sql, record)
 connection.commit()
-
 connection.close()
