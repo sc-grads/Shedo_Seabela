bddce28c7dbfb3a5a5bde5ab96ee2e5672c96994
SeabelaSHEDO <shedo.seabela@sambeconsulting.com>
2023-02-20 06:17:41 +0200

    deleting

--- Python_Bootcamp/SQL/SQL.py
+++ Python_Bootcamp/SQL/SQL.py
@@ -3,9 +3,14 @@ import sqlite3
 connection = sqlite3.connect('my_database.db')
 
 cursor = connection.cursor()
-id = input('Id:')
 
-sql = 'UPDATE employees SET department="Sales" where id=?;'
-cursor.execute(sql, (id,))
+
+department = input('Department:')
+
+sql = 'DELETE FROM employees where department = ?;'
+cursor.execute(sql, (department,))
+
+
 connection.commit()
+
 connection.close()
