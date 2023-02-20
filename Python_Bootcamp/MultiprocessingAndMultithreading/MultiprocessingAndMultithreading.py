27665643070063e3fac66d117193d8bdf5f69e36
SeabelaSHEDO <shedo.seabela@sambeconsulting.com>
2023-02-20 15:47:57 +0200

    Sharing Data using array

--- Python_Bootcamp/MultiprocessingAndMultithreading/MultiprocessingAndMultithreading.py
+++ Python_Bootcamp/MultiprocessingAndMultithreading/MultiprocessingAndMultithreading.py
@@ -1,25 +1,33 @@
 import multiprocessing as mp
-
-def increment(counter):
-    counter.value += 1
-
-def my_increment(my_counter):
-    my_counter += 1
-
+def squares(numbers, squares_list):
+    for n in numbers:
+        squares_list.append(n ** 2)
+    print(f'square_list inside process {squares_list}')
+def cubes(numbers, result):
+    i = 0
+    for num in numbers:
+        result[i] = num ** 3
+        i += 1
+    print(f'result Array inside process/function: {result[::]}')
 
 if __name__ == '__main__':
-    my_counter = 1
-    counter = mp.Value('i', 1)
-    process = mp.Process(target=increment, args=(counter,))
-    process.start()
-    process.join()
 
-    print(f'counter of type multiprocessing.Value is {counter.value}')
+    numbers = [1, 2, 3]
+    squares_list = list()
 
 
-    for i in range(10):
-        process = mp.Process(target=my_increment, args=(my_counter,))
-        process.start()
-        process.join()
+    p = mp.Process(target=squares, args=(numbers, squares_list))
+    p.start()
+    p.join()
 
-    print(f'my_counter of type integer is {my_counter}')
+
+    print(f'squares_list outsite process {squares_list}')
+
+    result = mp.Array('i', len(numbers))
+
+
+    p1 = mp.Process(target=cubes, args=(numbers, result))
+    p1.start()
+    p1.join()
+
+    print(f'result Array outside process {result[::]}')
