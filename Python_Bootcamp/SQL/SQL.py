8a3b8d80d94a098d493237dc7494bd9c379cf650
SeabelaSHEDO <shedo.seabela@sambeconsulting.com>
2023-02-20 06:03:39 +0200

    parameterized

--- Python_Bootcamp/SQL/SQL.py
+++ Python_Bootcamp/SQL/SQL.py
@@ -3,13 +3,18 @@ import sqlite3
 connection = sqlite3.connect('my_database.db')
 
 cursor = connection.cursor()
-
-sql = 'select * from employees'
-cursor.execute(sql)
+id = input('Enter ID:')
+sql = 'select * from employees where id = ?;'
+cursor.execute(sql, (id,))
 
 for row in cursor.fetchall():
     print(row)
 
+
+record = (10, 'Leonardo Romano', 'Marketing', '+40122111', 'leo@x.com')
+
+sql = 'insert into employees values (?,?,?,?,?);'
+cursor.execute(sql, record)
 connection.commit()
 
 connection.close()
