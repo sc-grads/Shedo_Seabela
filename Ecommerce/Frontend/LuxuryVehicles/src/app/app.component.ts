d1b55e62528899aeeda1648e651099d9af46ee74
SeabelaSHEDO <shedo.seabela@sambeconsulting.com>
2023-03-31 13:08:08 +0200

    Added routing

--- Ecommerce/Frontend/LuxuryVehicles/src/app/app.component.ts
+++ Ecommerce/Frontend/LuxuryVehicles/src/app/app.component.ts
@@ -1,10 +1,32 @@
+import { AppComponent } from './app.component';
 import { Component } from '@angular/core';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomePage } from './Pages/HomePage/HomePage.component'
+import { RegistrationPage } from './Pages/RegistrationPage/RegistrationPage.component'
 
+const routes: Routes = [
+  { path: '', component: HomePage },
+  { path: 'RegistrationPage', component: RegistrationPage ,},
+ ];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomePage,
+    RegistrationPage
+         ],
+  imports: [
+    AppRoutingModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'LuxuryVehicles';
+export class AppModule {
+
 }
