1ab29389d18bdeafa96d8b8d677deb8e0bf14a9c
SeabelaSHEDO <shedo.seabela@sambeconsulting.com>
2023-04-03 17:20:19 +0200

    PowerBI_File

--- Ecommerce/Frontend/LuxuryVehicles/src/app/app.component.ts
+++ Ecommerce/Frontend/LuxuryVehicles/src/app/app.component.ts
@@ -1,20 +1,13 @@
-import { AppComponent } from './app.component';
+
 import { Component } from '@angular/core';
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { HomePage } from './Pages/HomePage/HomePage.component'
-import { RegistrationPage } from './Pages/RegistrationPage/RegistrationPage.component'
 
-const routes: Routes = [
-  { path: '', component: HomePage },
-  { path: 'RegistrationPage', component: RegistrationPage ,},
- ];
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomePage,
-    RegistrationPage
+    AppComponent
+
          ],
   imports: [
     AppRoutingModule,
