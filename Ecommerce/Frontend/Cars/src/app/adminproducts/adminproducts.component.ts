abd47dd321937cf054df391284062e19287099ff
SeabelaSHEDO <shedo.seabela@sambeconsulting.com>
2023-05-24 11:32:43 +0200

    Added Products

---
+++ Ecommerce/Frontend/Cars/src/app/adminproducts/adminproducts.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-adminproducts',
+  templateUrl: './adminproducts.component.html',
+  styleUrls: ['./adminproducts.component.css']
+})
+export class AdminproductsComponent {
+
+}
