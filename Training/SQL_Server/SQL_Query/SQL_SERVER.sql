f8559eb4be5babe63495fb4403db9d5d4ab9c1f4
SeabelaSHEDO <shedo.seabela@sambeconsulting.com>
2023-09-01 15:08:07 +0200

    SQL_Queries

---
+++ Training/SQL_Server/SQL_Query/SQL_SERVER.sql
@@ -0,0 +1,159 @@
+﻿SELECT * FROM 
+DimProduct
+-- -- Declare the variable @region
+DECLARE @region VARCHAR(10)
+
+-- Update the variable value
+SET @region = 'RFC'
+
+--
+-- Declare your variables
+DECLARE @start DATE
+DECLARE @stop DATE
+DECLARE @affected INT;
+-- SET the relevant values for each variable
+SET @start = '2014-01-24'
+SET @stop  = '2014-07-02'
+SET @affected =  5000 ;
+-- ANOTHER 1111
+SELECT 
+  description,
+  nerc_region,
+  demand_loss_mw,
+  affected_customers
+FROM 
+  grid
+-- Specify the date range of the event_date and the value for @affected
+WHERE event_date BETWEEN @start AND @stop 
+AND affected_customers >= @affected;
+
+-- ANOTHER 1111
+SELECT  album.title AS album_title,
+  artist.name as artist,
+  MAX(track.milliseconds / (1000 * 60) % 60 ) AS max_track_length_mins
+-- Name the temp table #maxtracks
+INTO #maxtracks
+FROM album
+-- Join album to artist using artist_id
+INNER JOIN artist ON album.artist_id = artist.artist_id
+-- Join track to album using album_id
+INNER JOIN track 
+ON track.album_id=album.album_id
+GROUP BY artist.artist_id, album.title, artist.name,album.album_id
+-- Run the final SELECT query to retrieve the results from the temporary table
+SELECT album_title, artist, max_track_length_mins
+FROM  #maxtracks
+ORDER BY max_track_length_mins DESC, artist;
+
+-- ANOTHER 11
+-- Create a table for the professors entity type
+CREATE TABLE professors (
+ firstname text,
+ lastname text
+);
+
+-- Print the contents of this table
+SELECT * 
+FROM professors
+
+-- ANOTHER 111
+-- Rename the organisation column
+ALTER TABLE affiliations
+RENAME COLUMN organisation TO organization;
+
+-- Delete the university_shortname column
+ALTER TABLE affiliations
+DROP COLUMN university_shortname;
+
+-- ANOTHER 111
+
+-- Insert unique professors into the new table
+INSERT INTO professors 
+SELECT DISTINCT firstname, lastname, university_shortname 
+FROM university_professors;
+
+-- Doublecheck the contents of professors
+SELECT * 
+FROM professors;
+
+-- ANOTHER 111
+-- Delete the university_professors table
+DROP TABLE university_professors;
+
+ALTER TABLE professors
+ALTER COLUMN university_shortname
+TYPE CHAR(3);
+
+-- Convert the values in firstname to a max. of 16 characters
+ALTER TABLE professors 
+ALTER COLUMN firstname 
+TYPE varchar(16)
+USING SUBSTRING(firstname FROM 1 FOR 16);
+
+-- Make universities.university_shortname unique
+ALTER TABLE universities
+ADD CONSTRAINT universities_shortname_unq UNIQUE(universities.university_shortname);
+
+-- Rename the university_shortname column to id
+ALTER TABLE universities
+RENAME COLUMN university_shortname TO id;
+
+-- Make id a primary key
+ALTER TABLE organizations
+ADD CONSTRAINT university_pk PRIMARY KEY (id);
+
+-- Count the number of distinct rows with columns make, model
+SELECT COUNT(DISTINCT(make, model)) 
+FROM cars;
+
+-- Add the id column
+ALTER TABLE cars
+ADD COLUMN id varchar(128);
+
+-- Update id with make + model
+UPDATE cars
+SET id = CONCAT(make, model);
+
+
+
+-- Make id a primary key
+ALTER TABLE cars
+ADD CONSTRAINT id_pk PRIMARY KEY (id);
+
+-- Have a look at the table
+SELECT * FROM cars;
+
+-- Add a foreign key on professors referencing universities
+ALTER TABLE professors 
+ADD CONSTRAINT professors_fkey FOREIGN KEY (university_id) REFERENCES universities (id);
+
+-- Identify the correct constraint name
+SELECT constraint_name, table_name, constraint_type
+FROM information_schema.table_constraints
+WHERE constraint_type = 'FOREIGN KEY';
+
+-- Drop the right foreign key constraint
+ALTER TABLE affiliations
+DROP CONSTRAINT affiliations_organization_id_fkey;
+
+-- Add a new foreign key constraint from affiliations to organizations which cascades deletion
+ALTER TABLE affiliations
+ADD CONSTRAINT affiliations_organization_id_fkey FOREIGN KEY (organization_id) REFERENCES organizations (id) ON DELETE CASCADE;
+
+-- Delete an organization 
+DELETE FROM organizations 
+WHERE id = 'CUREM';
+
+-- Check that no more affiliations with this organization exist
+SELECT * FROM affiliations
+WHERE organization_id = 'CUREM';
+
+-- Calculate the aggregations by Shape
+SELECT Shape,
+       AVG(DurationSeconds) AS Average, 
+       MIN(DurationSeconds) AS Minimum, 
+       MAX(DurationSeconds) AS Maximum
+FROM Incidents
+GROUP BY Shape
+-- Return records where minimum of DurationSeconds is greater than 1
+HAVING MIN(DurationSeconds) > 1
